61c46ccb10a60fbd7a550f1135f248847999ba47
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-04-05 16:49:50 +0100

    pagination project files

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Function that returns page index size"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)
